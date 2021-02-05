723720c5ab46ea519df08ab67c059e2a054a72fe
D L <laomeister93@gmail.com>
2021-02-05 01:48:35 -0500

    grid and player movement

--- Assets/Scripts/InputHandling.cs
+++ Assets/Scripts/InputHandling.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class InputHandling : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float inputCooldown;
+    public Player player;
+
+    private float inputCooldownTimer = 0f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (inputCooldownTimer == 0f)
+        {
+            if (Input.GetAxis("Horizontal") < 0)
+            {
+                player.Move(Direction.LEFT);
+                inputCooldownTimer = inputCooldown;
+            }
+            else if (Input.GetAxis("Horizontal") > 0)
+            {
+                player.Move(Direction.RIGHT);
+                inputCooldownTimer = inputCooldown;
+            }
+            else if (Input.GetAxis("Vertical") < 0)
+            {
+                player.Move(Direction.DOWN);
+                inputCooldownTimer = inputCooldown;
+            }
+            else if (Input.GetAxis("Vertical") > 0)
+            {
+                player.Move(Direction.UP);
+                inputCooldownTimer = inputCooldown;
+            }
+        } else
+        {
+            inputCooldownTimer = Mathf.Max(inputCooldownTimer - Time.deltaTime, 0f);
+        }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,40 +13,107 @@ public class Player : MonoBehaviour
         get; private set;
     }
 
+    public float moveDurationSecs;
+    public GameObject camera;
+
+    private Direction currentMovement;
+    private bool isMoving = false;
+    private float elapsedMoveTime;
+    private float oldPlayerX;
+    private float oldPlayerY;
+
     private void Start()
     {
         PlayerX = 10;
         PlayerY = 10;
     }
 
+    private void Update()
+    {
+        if (isMoving)
+        {
+            if (elapsedMoveTime > moveDurationSecs)
+            {
+                isMoving = false;
+            } else
+            {
+                float displacement = elapsedMoveTime / moveDurationSecs;
+                switch (currentMovement)
+                {
+                    case Direction.UP:
+                        transform.position = new Vector3(
+                            transform.position.x,
+                            oldPlayerY + displacement,
+                            transform.position.z);
+                        break;
+                    case Direction.DOWN:
+                        transform.position = new Vector3(
+                            transform.position.x,
+                            oldPlayerY - displacement,
+                            transform.position.z);
+                        break;
+                    case Direction.LEFT:
+                        transform.position = new Vector3(
+                            oldPlayerX - displacement,
+                            transform.position.y,
+                            transform.position.z);
+                        break;
+                    case Direction.RIGHT:
+                        transform.position = new Vector3(
+                            oldPlayerX + displacement,
+                            transform.position.y,
+                            transform.position.z);
+                        break;
+                }
+                elapsedMoveTime += Time.deltaTime;
+            }
+        }
+        camera.transform.position = new Vector3(transform.position.x, transform.position.y, camera.transform.position.z);
+    }
+
     public void Move(Direction direction)
     {
+        bool isValidSignal = false;
+
+        // receive the move signal
         switch (direction)
         {
             case Direction.UP:
                 if (PlayerY > 0)
                 {
                     PlayerY--;
+                    isValidSignal = true;
                 }
                 break;
             case Direction.DOWN:
                 if (PlayerY < Constants.GRID_HEIGHT - 1)
                 {
                     PlayerY++;
+                    isValidSignal = true;
                 }
                 break;
             case Direction.LEFT:
                 if (PlayerX > 0)
                 {
                     PlayerX--;
+                    isValidSignal = true;
                 }
                 break;
             case Direction.RIGHT:
                 if (PlayerX < Constants.GRID_WIDTH - 1)
                 {
                     PlayerX++;
+                    isValidSignal = true;
                 }
                 break;
+        }
+        if (isValidSignal)
+        {
+            currentMovement = direction;
+            isMoving = true;
+            elapsedMoveTime = 0f;
+            oldPlayerX = transform.position.x;
+            oldPlayerY = transform.position.y;
         }
     }
 }
